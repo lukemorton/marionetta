3ee936a6aa6d8de56298feb1c6f06d030ff5f4ff
Luke Morton <lukemorton.dev@gmail.com>
2012-09-15 14:12:16 +0100

    Reorder methods.

--- lib/marionetta/manipulators.rb
+++ lib/marionetta/manipulators.rb
@@ -1,15 +1,15 @@
 module Marionetta
   module Manipulators
-    def [](key)
-      all[key.to_s]
-    end
-    
     require_relative 'manipulators/puppet_manipulator'
 
     def all()
       {
         :puppet => PuppetManipulator,
       }
+    end
+    
+    def [](key)
+      all[key.to_s]
     end
   end
 end
