f00d412365cf4ed6c0eecb247b650d8742fb233d
Luke Morton <lukemorton.dev@gmail.com>
2012-09-15 14:48:18 +0100

    Whitespace.

--- lib/marionetta.rb
+++ lib/marionetta.rb
@@ -1,5 +1,5 @@
 module Marionetta
-  VERSION = '0.1.0'
+  VERSION     = '0.1.0'
   DESCRIPTION = 'For lightweight puppet mastery. Organise
                  multiple machines via rsync and SSH rather
                  than using puppet master'
