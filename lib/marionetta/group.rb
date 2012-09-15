6ccc33cbd492c8327c4c989e0eb0e36311b8a8bf
Luke Morton <lukemorton.dev@gmail.com>
2012-09-15 12:31:34 +0100

    Restructure.

--- lib/marionetta/group.rb
+++ lib/marionetta/group.rb
@@ -1,27 +1,36 @@
-class Marionetta::Group
-  attr_reader :servers
+module Marionetta
+  class Group
+    attr_reader :servers
 
-  def initialize()
-    @servers = []
-  end
+    def initialize()
+      @servers = []
+    end
 
-  def add_server()
-    server = {}
-    yield server
-    @servers << server
-  end
-
-  def add_servers(range)
-    range.each do |i|
+    def add_server()
       server = {}
-      yield server, i
+      yield server
       @servers << server
     end
-  end
 
-  def each_server()
-    servers.each do |s|
-      yield s
+    def add_servers(range)
+      range.each do |i|
+        server = {}
+        yield server, i
+        @servers << server
+      end
+    end
+
+    def each_server()
+      servers.each do |s|
+        yield s
+      end
+    end
+
+    def manipulate_each_server(manipulator_name, method)
+      each_server do |s|
+        manipulator = Manipulators[manipulator_name].new(s)
+        manipulator.method(method).call()
+      end
     end
   end
 end
