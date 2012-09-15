f0d8fe2e7cf44cc167ceda67d402a0f4f0eed4e9
Luke Morton <lukemorton.dev@gmail.com>
2012-09-15 12:32:03 +0100

    Add real spec as mentioned in README.

--- spec/marionetta_spec.rb
+++ spec/marionetta_spec.rb
@@ -1,7 +1,53 @@
-describe Marionetta
+# $:.unshift(File.dirname(__FILE__))
 
-  it 'defines a server' do
-  	true.should == true
+require 'marionetta'
+require 'vagrant'
+
+describe Marionetta do
+  it 'can update puppet without a master on multiple nodes asynchronously' do
+    def apply_defaults(s)
+      s[:username] = 'ubuntu'
+      s[:puppet] = {:modules => 'puppet/modules'}
+    end
+
+    staging = Marionetta::Group.new
+    staging.add_server do |s|
+      apply_defaults(s)
+
+      s[:hostname] = 'staging.example.com'
+      s[:puppet][:manifest] = 'puppet/manifests/staging.pp'
+    end
+    
+    production = Marionetta::Group.new
+    production.add_servers (1..2) do |s, i|
+      apply_defaults(s)
+
+      s[:hostname] = "prod-#{i}.example.com"
+      s[:puppet][:manifest] = 'puppet/manifests/production-web.pp'
+    end
+
+    # all = Marionetta::Group.new(staging, production)
+    # all.manipulate_each_server(:puppet, :update)
+
+    # Boot up vagrant for testing
+    env = Vagrant::Environment.new(:cwd => File.dirname(__FILE__)+'/vagrant')
+    env.cli('up')
+
+    # Manipulate one server
+    puppet = Marionetta::Manipulators::PuppetManipulator.new({
+      :hostname => 'vagrant@192.168.33.11',
+      :puppet   => {:manifest => 'puppet/manifest.pp'}
+    })
+    puppet.update
+
+    # Manipulate a group of servers
+    vagrant = Marionetta::Group.new
+    vagrant.add_server do |s|
+      apply_defaults(s)
+
+      s[:hostname] = 'vagrant@192.168.33.11'
+      s[:puppet] = {:manifest => 'puppet/manifest.pp'}
+    end
+    vagrant.manipulate_each_server(:puppet, :update)
   end
-
 end
