80dd490dd7bbd4bd8788ed2275f151c117ffb433
Luke Morton <lukemorton.dev@gmail.com>
2012-09-15 12:31:23 +0100

    Include submodules and classes.

--- lib/marionetta.rb
+++ lib/marionetta.rb
@@ -3,4 +3,8 @@ module Marionetta
   DESCRIPTION = 'For lightweight puppet mastery. Organise
                  multiple machines via rsync and SSH rather
                  than using puppet master'
+
+  require_relative 'marionetta/group'
+  require_relative 'marionetta/ssh'
+  require_relative 'marionetta/manipulators'
 end
