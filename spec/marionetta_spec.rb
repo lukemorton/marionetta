26f5426f25359edc2fae64609bf689e708fd5225
Luke Morton <lukemorton.dev@gmail.com>
2012-09-16 16:42:52 +0100

    Remove a lot of junk.

--- spec/marionetta_spec.rb
+++ spec/marionetta_spec.rb
@@ -1,6 +1,9 @@
 require 'marionetta'
 require 'vagrant'
 
+env = Vagrant::Environment.new(:cwd => File.dirname(__FILE__)+'/vagrant')
+env.cli('up')
+
 describe Marionetta do
   it 'can update puppet without a master on multiple nodes asynchronously' do
     def apply_defaults(s)
@@ -10,35 +13,9 @@ describe Marionetta do
 
     staging = Marionetta::Group.new
     staging.add_server do |s|
-      apply_defaults(s)
-
-      s[:hostname] = 'staging.example.com'
-      s[:puppet][:manifest] = File.dirname(__FILE__)+'/puppet/manifests/staging.pp'
-    end
-    
-    production = Marionetta::Group.new
-    production.add_servers (1..2) do |s, i|
-      apply_defaults(s)
-
-      s[:hostname] = "prod-#{i}.example.com"
-      s[:puppet][:manifest] = File.dirname(__FILE__)+'/puppet/manifests/production-web.pp'
-    end
-
-    # all = Marionetta::Group.new(staging, production)
-    # all.manipulate_each_server(:puppet, :update)
-
-    # Boot up vagrant for testing
-    env = Vagrant::Environment.new(:cwd => File.dirname(__FILE__)+'/vagrant')
-    env.cli('up')
-
-    # Manipulate a group of servers
-    vagrant = Marionetta::Group.new
-    vagrant.add_server do |s|
-      apply_defaults(s)
-
       s[:hostname] = 'vagrant@192.168.33.11'
       s[:puppet] = {:manifest => File.dirname(__FILE__)+'/puppet/manifest.pp'}
     end
-    vagrant.manipulate_each_server(:puppet, :update)
+
   end
 end
