79b5acf96da4835e11023864b0a5861d21a0ffa3
Luke Morton <lukemorton.dev@gmail.com>
2012-09-16 16:45:17 +0100

    Test should provide a default SSH map.

--- spec/marionetta_spec.rb
+++ spec/marionetta_spec.rb
@@ -5,17 +5,7 @@ env = Vagrant::Environment.new(:cwd => File.dirname(__FILE__)+'/vagrant')
 env.cli('up')
 
 describe Marionetta do
-  it 'can update puppet without a master on multiple nodes asynchronously' do
-    def apply_defaults(s)
-      s[:username] = 'ubuntu'
-      s[:puppet] = {:modules => File.dirname(__FILE__)+'/puppet/modules'}
-    end
-
-    staging = Marionetta::Group.new
-    staging.add_server do |s|
-      s[:hostname] = 'vagrant@192.168.33.11'
-      s[:puppet] = {:manifest => File.dirname(__FILE__)+'/puppet/manifest.pp'}
-    end
-
+  it 'should provide a default SSH map' do
+    Marionetta.default_server
   end
 end
