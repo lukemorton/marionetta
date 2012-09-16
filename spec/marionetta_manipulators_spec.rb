a154606b01e2c3f3f353e456abaf072425b6d660
Luke Morton <lukemorton.dev@gmail.com>
2012-09-16 17:04:08 +0100

    No need to output.

--- spec/marionetta_manipulators_spec.rb
+++ spec/marionetta_manipulators_spec.rb
@@ -2,6 +2,6 @@ require 'marionetta/manipulators'
 
 describe Marionetta::Manipulators do
   it 'should maintain a list of manipulators' do
-    puts Marionetta::Manipulators.all
+    Marionetta::Manipulators.all
   end
 end
