f398dae5a708ca59957f1bd3f2dd991dd718554f
Luke Morton <lukemorton.dev@gmail.com>
2012-09-16 16:25:30 +0100

    .rsync and .run should provide more options via server.

--- lib/marionetta/ssh.rb
+++ lib/marionetta/ssh.rb
@@ -20,11 +20,28 @@ module Marionetta
     end
 
     def rsync(from, to)
-      system("rsync", "-azP", "--delete", from, to)
+      rsync_cmd = [server[:rsync][:command]]
+
+      if server[:rsync].has_key?(:flags)
+        rsync_cmd << server[:rsync][:flags]
+      end
+      
+      rsync_cmd << [from, to]
+
+      system(*rsync_cmd.flatten)
     end
 
     def run(command)
-      system("ssh", server[:hostname], command)
+      ssh_cmd = [server[:ssh][:command]]
+
+      if server[:ssh].has_key?(:flags)
+        ssh_cmd << server[:ssh][:flags]
+      end
+
+      ssh_cmd << server[:hostname]
+      ssh_cmd << command
+
+      system(*ssh_cmd.flatten)
     end
   end
 end
