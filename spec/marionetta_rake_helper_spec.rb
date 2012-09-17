e68201767629053aa53779a997ac8bfcfc93989a
Luke Morton <lukemorton.dev@gmail.com>
2012-09-17 12:49:12 +0100

    Test rake tasks created.

--- spec/marionetta_rake_helper_spec.rb
+++ spec/marionetta_rake_helper_spec.rb
@@ -5,5 +5,6 @@ describe Marionetta::RakeHelper do
   it 'should install rake tasks' do
     group = Marionetta::Group.new(:staging)
     Marionetta::RakeHelper.new(group).install_group_tasks
+    Rake::Task.tasks.count.should > 0
   end
 end
