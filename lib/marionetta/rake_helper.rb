a7bfd7f24d98e98b5f94d8b71ec49c17eca83835
Luke Morton <lukemorton.dev@gmail.com>
2012-09-17 14:05:30 +0100

    Improve task descriptions.

--- lib/marionetta/rake_helper.rb
+++ lib/marionetta/rake_helper.rb
@@ -24,7 +24,7 @@ module Marionetta
     def install_group_tasks_for(group)
       Manipulators.all.each do |manipulator_name, manipulator_class|
         manipulator_class.tasks.each do |method_name|
-          desc("#{manipulator_name} #{method_name} (marionetta)")
+          desc("#{method_name} #{manipulator_name} on #{group.name} (marionetta)")
           task(task_name(group, manipulator_name, method_name)) do
             group.manipulate_each_server(manipulator_name, method_name)
           end
