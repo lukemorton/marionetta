919dcec6c8e6951724fbd8a7fdadba3e66eaea3a
Luke Morton <lukemorton.dev@gmail.com>
2012-09-17 12:49:54 +0100

    Assertions are good :)

--- spec/marionetta_manipulators_spec.rb
+++ spec/marionetta_manipulators_spec.rb
@@ -2,6 +2,6 @@ require 'marionetta/manipulators'
 
 describe Marionetta::Manipulators do
   it 'should maintain a list of manipulators' do
-    Marionetta::Manipulators.all
+    Marionetta::Manipulators.all.count.should > 0
   end
 end
