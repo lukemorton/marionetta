f06350fbbba3937618773e2b66ed278ebc7c4952
Luke Morton <lukemorton.dev@gmail.com>
2012-09-17 13:02:38 +0100

    New release v0.1.9.

--- lib/marionetta.rb
+++ lib/marionetta.rb
@@ -1,5 +1,5 @@
 module Marionetta
-  VERSION     = '0.1.8'
+  VERSION     = '0.1.9'
   DESCRIPTION = 'For lightweight puppet mastery. Organise
                  multiple machines via rsync and SSH rather
                  than using puppet master'
