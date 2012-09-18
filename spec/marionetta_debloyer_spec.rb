ed3152399d3d01e04c2c67c119ea17c1dca0382b
Luke Morton <lukemorton.dev@gmail.com>
2012-09-18 14:31:31 +0100

    Test deployment.

--- spec/marionetta_debloyer_spec.rb
+++ spec/marionetta_debloyer_spec.rb
@@ -1,25 +1,22 @@
 require 'spec_helper'
+require "marionetta"
 require "marionetta/manipulators/debloyer"
 
 describe Marionetta::Manipulators::Debloyer do
   it 'should deploy a deb' do
     ssh_key_path = File.dirname(__FILE__)+'/vagrant/key'
 
-    server = Marionetta.default_server
-    server[:debloyer] = {
-      :from => File.dirname(__FILE__)+'/app',
-      :fpm => {
-        :command => 'fpm',
-        :flags => [
-          '-n', 'test',
-          '-s', 'dir',
-          '-t', 'deb',
-          '--architecture', 'amd64',
-          '--prefix',       '/tmp',
-        ],
-      },
-    }
+    s = Marionetta.default_server
 
-    Marionetta::Manipulators::Debloyer.new(server).build
+    s[:hostname] = 'vagrant@192.168.33.11'
+    ssh_key_path = File.dirname(__FILE__)+'/vagrant/key'
+    s[:ssh][:flags] = ['-i', ssh_key_path]
+    s[:rsync][:flags] = ['-azP', '-e', "ssh -i #{ssh_key_path}", '--delete']
+
+    s[:debloyer][:from] = File.dirname(__FILE__)+'/app'
+    s[:debloyer][:to] = '/home/vagrant'
+    s[:debloyer][:name] = 'test'
+
+    Marionetta::Manipulators::Debloyer.new(s).deploy
   end
 end
