2c648f468620a9bf9e4438600f79c61e631e04dd
Luke Morton <lukemorton.dev@gmail.com>
2012-09-19 10:57:42 +0100

    Test rollback.

--- spec/marionetta_deployer_spec.rb
+++ spec/marionetta_deployer_spec.rb
@@ -7,11 +7,15 @@ def deployer()
 end
 
 describe Marionetta::Manipulators::Deployer do
-  it 'should deploy a deb' do
+  it 'should deploy' do
     deployer.deploy
   end
 
   it 'should list releases' do
     deployer.releases.length.should > 0
+  end
+
+  it 'should rollback' do
+    deployer.rollback
   end
 end
