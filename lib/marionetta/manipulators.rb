c95daba9ca82a08b030c7a5ebbaeda846b042479
Luke Morton <lukemorton.dev@gmail.com>
2012-09-23 20:16:22 +0100

    Comment on Manipulators.

--- lib/marionetta/manipulators.rb
+++ lib/marionetta/manipulators.rb
@@ -1,9 +1,25 @@
+# `Manipulators` is a container for registering manipulators.
+# 
+# The interface of a manipulator is:
+# 
+#     self.tasks()        an array of methods to expose via
+#                         RakeHelpers *optional*
+# 
+#     initialize(server)  *required*
+#     can?()              *required*
+#     
 module Marionetta
   module Manipulators
+
+    # We automatically require the manipulators packaged with
+    # this library. *Is this a good idea?*
+    # 
     require_relative 'manipulators/deployer'
     require_relative 'manipulators/debloyer'
     require_relative 'manipulators/puppet_manipulator'
 
+    # A hash of all the manipulators.
+    # 
     def self.all()
       {
         :deployer => Deployer,
@@ -12,6 +28,8 @@ module Marionetta
       }
     end
 
+    # Get a manipulator.
+    # 
     def self.[](key)
       all[key]
     end
