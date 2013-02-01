b87cf022a09842871790fa82fc851b442c5efcfe
Luke Morton <lukemorton.dev@gmail.com>
2013-02-01 17:02:17 +0000

    Whoops, this was also required in rename of Puppet manipulator.

--- spec/puppet_manipulator_spec.rb
+++ spec/puppet_manipulator_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require_relative '../lib/marionetta/manipulators/puppet_manipulator'
 
-describe Marionetta::Manipulators::PuppetManipulator do
+describe Marionetta::Manipulators::Puppet do
   it 'should manipulate one server map' do
     Marionetta::Manipulators::Puppet.new(server).update
   end
