93a2f9262dbd752a03385d45167b2e9a418d2a29
Luke Morton <lukemorton.dev@gmail.com>
2013-02-07 15:10:44 +0000

    Add Marionetta::DirectorySync.

---
+++ lib/marionetta/directory_sync.rb
@@ -0,0 +1,56 @@
+require 'marionetta/commandable'
+
+module Marionetta
+  class DirectorySync
+    include Commandable
+
+    def self.sync(server, from, to, options)
+      new(server).sync(from, to, options)
+    end
+
+    def initialize(server)
+      @server = server
+    end
+
+    def sync(from, to, options)
+      create_dir(to)
+      sync_dir(from, to, options[:exclude])
+    end
+
+  private
+
+    def create_dir(dir)
+      cmd.ssh("test -d #{dir} || mkdir -p #{dir}")
+    end
+
+    def rsync_exclude_files(from, exclude_files)
+      exclude_files = exclude_files.clone
+
+      exclude_files.map! {|f| Dir["#{from}/#{f}"]}
+      exclude_files.flatten!
+
+      exclude_files.map! {|f| f.sub(from + '/', '')}
+
+      return exclude_files
+    end
+
+    def rsync_exclude_flags(exclude_files)
+      exclude_files.map! {|f| ['--exclude', f]}
+      exclude_files.flatten!
+
+      return exclude_files
+    end
+
+    def sync_dir(from, to, exclude_files = [])
+      args = [Dir[from+'/*'], to]
+
+      unless exclude_files.empty?
+        args.concat(rsync_exclude_flags(rsync_exclude_files(from, exclude_files)))
+      end
+
+      unless cmd.put(*args)
+        fatal('Could not rsync cache dir')
+      end
+    end
+  end
+end
